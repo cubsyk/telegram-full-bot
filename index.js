d9ea3b2c2afac6c263ca6af2685506e914cf52ea
cubsyk <akunbayu18@gmail.com>
2026-02-25 17:51:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+const botUsername = "tesjk2_bot"; // TANPA @
+const channelUsername = "@alfamartjk2"; // PAKE @
+const ADMIN_ID = 1388479642; // NANTI GANTI CHAT ID KAMU
+
+// Load database
+let database = {};
+if (fs.existsSync('database.json')) {
+    database = JSON.parse(fs.readFileSync('database.json'));
+}
+
+// Generate kode random
+function generateCode() {
+    return "vid" + Math.floor(100000 + Math.random() * 900000);
+}
+
+// Simpan database
+function saveDatabase() {
+    fs.writeFileSync('database.json', JSON.stringify(database, null, 2));
+}
+
+// ==========================
+// UPLOAD VIDEO (ADMIN ONLY)
+// ==========================
+bot.on('message', (msg) => {
+
+    if (msg.video && msg.chat.id === ADMIN_ID) {
+
+        const file_id = msg.video.file_id;
+        const kode = generateCode();
+
+        database[kode] = file_id;
+        saveDatabase();
+
+        const link = `https://t.me/${botUsername}?start=${kode}`;
+
+        bot.sendMessage(msg.chat.id,
+            `✅ Video berhasil disimpan!\n\n🔗 Link:\n${link}`
+        );
+    }
+});
+
+// ==========================
+// HANDLE DEEP LINK
+// ==========================
+bot.onText(/\/start (.+)/, async (msg, match) => {
+
+    const chatId = msg.chat.id;
+    const kode = match[1];
+
+    if (!database[kode]) {
+        return bot.sendMessage(chatId, "❌ Video tidak ditemukan.");
+    }
+
+    try {
+        const member = await bot.getChatMember(channelUsername, chatId);
+
+        if (member.status === "left") {
+            return bot.sendMessage(chatId,
+                `🚫 Kamu harus join dulu ke channel:\nhttps://t.me/${channelUsername.replace("@","")}`
+            );
+        }
+
+        bot.sendVideo(chatId, database[kode]);
+
+    } catch (error) {
+        bot.sendMessage(chatId, "Terjadi kesalahan.");
+    }
+});
+
+// Start biasa
+bot.onText(/\/start$/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Upload video (admin only).");
+});
